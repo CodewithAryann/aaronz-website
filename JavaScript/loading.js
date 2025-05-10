04e061f25d3074dbcd9e377e7d321e2c6a6103bd
Aryan <151847516+CodewithAryann@users.noreply.github.com>
2025-05-10 13:37:31 +0400

    1

--- JavaScript/loading.js
+++ JavaScript/loading.js
@@ -6,3 +6,20 @@ function loaderAnimation() {
 }
 
 loaderAnimation();
+
+
+  window.addEventListener('DOMContentLoaded', () => {
+    const loader = document.getElementById('loader');
+
+    // Check if loader has been shown before in this session
+    if (sessionStorage.getItem('loaderShown')) {
+      loader.style.display = 'none'; // Hide loader instantly
+    } else {
+      sessionStorage.setItem('loaderShown', 'true');
+      // Wait for the full animation duration, then hide it
+      setTimeout(() => {
+        loader.classList.add('hidden');
+      }, 5000); // Adjust to match the total animation time
+    }
+  });
+
